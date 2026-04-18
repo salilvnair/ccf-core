6b4b5967fbe4fcf036db7dd824253c634136ec36
salilvnair <salilvnairgithub@gmail.com>
2026-04-18 02:06:21 -0500

    Update version to 1.0.4

--- src/main/java/com/github/salilvnair/ccf/config/hibernate/CcfPhysicalNamingStrategy.java
+++ src/main/java/com/github/salilvnair/ccf/config/hibernate/CcfPhysicalNamingStrategy.java
@@ -1,15 +1,25 @@
 package com.github.salilvnair.ccf.config.hibernate;
 
-import lombok.RequiredArgsConstructor;
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+import java.util.Collections;
 import java.util.List;
 
-@RequiredArgsConstructor
 public class CcfPhysicalNamingStrategy implements PhysicalNamingStrategy {
 
     private final List<PhysicalNamingStrategy> delegates;
+
+
+    /** Hibernate 7 instantiates strategies via their no-arg constructor.
+     *  Pass-through behaviour when there are no configured delegates. */
+    public CcfPhysicalNamingStrategy() {
+        this(Collections.emptyList());
+    }
+
+    public CcfPhysicalNamingStrategy(List<PhysicalNamingStrategy> delegates) {
+        this.delegates = (delegates == null) ? Collections.emptyList() : delegates;
+    }
 
 
     @Override
